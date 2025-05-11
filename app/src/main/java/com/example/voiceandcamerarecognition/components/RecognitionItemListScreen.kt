518b65bf2e8d707c4f8d2293a71aae44189a120a
Masato Uehara <masato.uehara.1975@gmail.com>
2025-05-11 22:43:34 +0900

    refactor: remove unused import
    The commit removes an unused import from `RecognitionItemListScreen.kt`.

--- app/src/main/java/com/example/voiceandcamerarecognition/components/RecognitionItemListScreen.kt
+++ app/src/main/java/com/example/voiceandcamerarecognition/components/RecognitionItemListScreen.kt
@@ -8,12 +8,10 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
 import com.example.voiceandcamerarecognition.ui.theme.VoiceAndCameraRecognitionTheme
 import com.example.voiceandcamerarecognition.schemas.RecognitionItem
 import com.example.voiceandcamerarecognition.viewmodels.RecognitionItemViewModel
